3a706daee33ca9b046387ac080a14b30367e1c61
Jakub Solarek <j.solarek@wp.pl>
2022-06-12 16:50:12 +0200

    Dodaj pliki projektów.

---
+++ KLIENT/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace KLIENT
+{
+    public partial class MainWindow : Window
+    {
+        TcpClient tcpclnt;
+        Stream stm;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wyslij();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            String str = wiadXAML.Text;
+            stm = tcpclnt.GetStream();
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            statusXAML.Text = "Transmitowanie";
+            stm.Write(ba, 0, ba.Length);
+            //tcpclnt.Close();
+        }
+        void wyslij()
+        {
+            int port = Convert.ToInt32(portXAML.Text);
+            tcpclnt = new TcpClient();
+            statusXAML.Text = "Łączenie";
+            tcpclnt.Connect(ipXAML.Text, port);
+            statusXAML.Text = "Połączono";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            tcpclnt.Close();
+            statusXAML.Text = "Rozłączono";
+        }
+    }
+}
---
+++ SERWER/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SERWER
+{
+    public partial class MainWindow : Window
+    { 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            start();
+        }
+        TcpListener myList;
+        Socket s;
+        public void start()
+        {
+            IPAddress ipAd = IPAddress.Parse(ipXAML.Text);
+            int port = Convert.ToInt32(portXAML.Text);
+            myList = new TcpListener(ipAd, port);
+            myList.Start();
+            status.Text = "Uruchomiono";
+        }
+        public void odbierz()
+        { 
+            s = myList.AcceptSocket();
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            for (int i = 0; i < k; i++)
+                tekstXAML.Text += Convert.ToChar(b[i]);
+            s.Close();
+            tekstXAML.Text += "\n";
+            //myList.Stop();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            odbierz();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            status.Text = "Wyłączono";
+            myList.Stop();
+            
+        }
+    }
+}
